316ec52b5e882b4fb5a89f54994c3b7e4fe973fc
Chris Marsh <c.marshian@gmail.com>
2019-09-30 23:17:03 -0700

    Enumerate devices

--- DinputTester.cpp
+++ DinputTester.cpp
@@ -1,20 +1,44 @@
-// DinputTester.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+#define WIN32_LEAN_AND_MEAN
+#define VC_EXTRALEAN
+#include <windows.h>
+
+#define DIRECTINPUT_VERSION 0x0800
+#include <dinput.h>
+#pragma comment(lib, "dinput8")
+#pragma comment(lib, "dxguid")
+
+BOOL CALLBACK EnumDevicesCb(LPCDIDEVICEINSTANCE diDevice, LPVOID)
+{
+    wchar_t guid[40];
+    if (StringFromGUID2(diDevice->guidProduct, guid, ARRAYSIZE(guid)))
+    {
+        std::wcout
+            << L"Name: " << diDevice->tszProductName << L"\n"
+            << L"GUID: " << guid << L"\n";
+    }
+    else {
+        std::wcerr << L"Could not extract GUID for " << diDevice->tszProductName << L"\n";
+    }
+    return DIENUM_CONTINUE;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    IDirectInput8* di8;
+    HRESULT hr = DirectInput8Create(GetModuleHandleW(nullptr), DIRECTINPUT_VERSION, IID_IDirectInput8, (void**)&di8, nullptr);
+    if (FAILED(hr))
+    {
+        std::cerr << "Could not create DirectInput 8 handle\n";
+        return 1;
+    }
+
+    if (di8->EnumDevices(DI8DEVCLASS_ALL, EnumDevicesCb, nullptr, DIEDFL_ATTACHEDONLY) != DI_OK)
+    {
+        std::cerr << "Could not enumerate devices.\n";
+        return 2;
+    }
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
