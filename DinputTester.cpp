7ee84678e375c9041eb8f12604978cad33962781
Chris Marsh <c.marshian@gmail.com>
2019-10-01 00:00:20 -0700

    Added timing

--- DinputTester.cpp
+++ DinputTester.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <chrono>
 
 #define WIN32_LEAN_AND_MEAN
 #define VC_EXTRALEAN
@@ -9,14 +10,21 @@
 #pragma comment(lib, "dinput8")
 #pragma comment(lib, "dxguid")
 
+using perf_clock = std::chrono::high_resolution_clock;
+static perf_clock::time_point last_time;
+
 BOOL CALLBACK EnumDevicesCb(LPCDIDEVICEINSTANCE diDevice, LPVOID)
 {
     wchar_t guid[40];
     if (StringFromGUID2(diDevice->guidProduct, guid, ARRAYSIZE(guid)))
     {
+        auto now = perf_clock::now();
+        auto duration_ms = std::chrono::duration<double, std::milli>(now - last_time);
+        last_time = now;
         std::wcout
             << L"Name: " << diDevice->tszProductName << L"\n"
-            << L"GUID: " << guid << L"\n";
+            << L"GUID: " << guid << L"\n"
+            << L"Enum time: " << duration_ms.count() << L" ms\n\n";
     }
     else {
         std::wcerr << L"Could not extract GUID for " << diDevice->tszProductName << L"\n";
@@ -34,11 +42,16 @@ int main()
         return 1;
     }
 
+    last_time = perf_clock::now();
+
     if (di8->EnumDevices(DI8DEVCLASS_ALL, EnumDevicesCb, nullptr, DIEDFL_ATTACHEDONLY) != DI_OK)
     {
         std::cerr << "Could not enumerate devices.\n";
         return 2;
     }
+
+    di8->Release();
+    di8 = nullptr;
 
     return 0;
 }
